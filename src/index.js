a55c480e47d9a9d4728cc473b4fc62d4960eb568
setcain <charlie.bronson00@gmail.com>
2020-12-07 15:13:21 -0600

    updates prompt validation

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
-const budgetUser = prompt('What is your weekly budget')
-let budgetAmount
+const form = document.getElementById("agregar-gasto");
+const listGroup = document.querySelector(".list-group");
+let budgetAmount;
 
+document.addEventListener("DOMContentLoaded", function () {
+  const budgetUser = prompt("What is your weekly budget");
+
+  budgetUser === "" ||
+  budgetUser === null ||
+  isNaN(budgetUser) ||
+  budgetUser <= 0
+    ? window.location.reload()
+    : console.log(true);
+});
 
 class Budget {
   constructor(budget) {
-    this.budget = Number(budget) // transform prompt value into Number
-    this.remaining = Number(budget)
+    this.budget = Number(budget); // transform prompt value into Number
+    this.remaining = Number(budget);
   }
 
-  budgetRemaining(amount = 0) { // default = 0
-    return this.remaining -= Number(amount)
+  budgetRemaining(amount = 0) {
+    return (this.remaining -= Number(amount));
   }
 }
-
 
 class Inteface {
   insertBudget(amount) {
-    const budgetSpan = document.querySelector('span#total')
-    const remainingSpan = document.querySelector('span#restante')
+    const budgetSpan = document.querySelector("span#total");
+    const remainingSpan = document.querySelector("span#restante");
 
-    budgetSpan.innerHTML = `${amount}`
-    remainingSpan.innerHTML = `${amount}`
+    budgetSpan.innerHTML = `${amount}`;
+    remainingSpan.innerHTML = `${amount}`;
   }
 }
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  if (budgetUser === null || budgetUser === '') {
-    window.location.reload() // location = host
-  } else {
-    budgetAmount = new Budget(budgetUser)
-    const ui = new Inteface()
-    ui.insertBudget(budgetAmount.budget)
-  }
-})
