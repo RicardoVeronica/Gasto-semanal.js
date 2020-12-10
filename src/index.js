df73a3d91eb176d3657bbfe05c08e9cba6a1ab69
setcain <charlie.bronson00@gmail.com>
2020-12-10 14:23:25 -0600

    restart all index.js adds prompt validations and classes

--- src/index.js
+++ src/index.js
@@ -1,120 +1,56 @@
-const form = document.getElementById("agregar-gasto");
-const listGroup = document.querySelector(".list-group"); // html dinamico
-let userAmount;
+/*
+ * Variables
+ * */
+const formulario = document.getElementById("agregar-gasto");
+const gastoListado = document.querySelector(".list-group"); // HTML dinamico
+let presupuesto; // Se usa en preguntarPresupuesto()
 
-document.addEventListener("DOMContentLoaded", function () {
-  const budgetUser = prompt("What is your weekly budget");
+/*
+ * Eventos
+ * */
+eventListeners();
+function eventListeners() {
+  // Dispara los event listeners
+  document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+}
 
-  if (
-    budgetUser === "" ||
-    budgetUser === null ||
-    isNaN(budgetUser) ||
-    budgetUser <= 0
-  ) {
-    window.location.reload();
-  }
-
-  userAmount = new BudgetClass(budgetUser);
-  userInterface.insertBudget(userAmount);
-});
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const expenseName = document.getElementById("gasto").value;
-  const expenseAmount = Number(document.getElementById("cantidad").value);
-
-  if (expenseName === "" || expenseAmount === "") {
-    userInterface.printAlert("Ambos campos son obligatorios", "error");
-    return;
-  } else if (expenseAmount <= 0 || isNaN(expenseAmount)) {
-    userInterface.printAlert("Cantidad no valida", "error");
-    return;
-  }
-
-  // Genera objeto con los datos validados
-  const userExpense = { expenseName, expenseAmount, id: Date.now() }; // object literal enhancement
-
-  userAmount.newExpense(userExpense); // Adds new expense in array
-  userInterface.printAlert("Gasto agregado correctamente", "succes"); // send message ok
-
-  const { expenses } = userAmount;
-  userInterface.printExpenses(expenses); // print expenses in html
-
-  form.reset();
-});
-
-class BudgetClass {
+/*
+ * Clases
+ * */
+class Presupuesto {
   // Crea presupuesto, restante y gastos
-  constructor(budget) {
-    this.budget = Number(budget);
-    this.remaining = Number(budget);
-    this.expenses = [];
-  }
-
-  newExpense(userExpense) {
-    this.expenses = [...this.expenses, userExpense];
+  constructor(presupuesto) {
+    this.presupuesto = Number(presupuesto); // Transforma a numero el presupuestoUsuario (prompt)
+    this.restante = Number(presupuesto); // Restante al inicio es lo mismo que presupuesto
+    this.gastos = [];
   }
 }
 
 class UserInterface {
-  // Inserta el presupuesto y el restante en el html
-  insertBudget(amountInterface) {
-    const { budget, remaining } = amountInterface;
-
-    document.getElementById("total").textContent = budget;
-    document.getElementById("restante").textContent = remaining;
-  }
-
-  cleanHTML() {
-    // Clean listGroup
-    while (listGroup.firstChild) {
-      listGroup.removeChild(listGroup.firstChild);
-    }
-  }
-
-  printAlert(msg, type) {
-    const div = document.createElement("div");
-    div.classList.add("text-center", "alert");
-
-    type === "error"
-      ? div.classList.add("alert-danger")
-      : div.classList.add("alert-success");
-
-    div.textContent = msg;
-    document.querySelector(".primario").insertBefore(div, form);
-
-    setTimeout(() => {
-      div.remove();
-    }, 2500);
-  }
-
-  printExpenses(expenses) {
-    this.cleanHTML();
-
-    expenses.forEach((expense) => {
-      const { expenseAmount, expenseName, id } = expense;
-
-      const li = document.createElement("li");
-      li.classList.add(
-        "list-group-item",
-        "d-flex",
-        "justify-content-between",
-        "align-items-center"
-      );
-      // li.setAttribute("data-id", id); // old way
-      li.dataset.id = id; // new way
-      li.innerHTML = `${expenseName} <span class="badge badge-primary badge-pill">$${expenseAmount}</span>`;
-
-      const btnDelete = document.createElement("button");
-      btnDelete.classList.add("btn", "btn-danger", "borrar-gasto");
-      btnDelete.textContent = "borrar";
-
-      li.appendChild(btnDelete);
-
-      listGroup.appendChild(li);
-    });
-  }
+  // Muestra HTML necesario
 }
 
+/*
+ * Instancias de clases
+ * */
 const userInterface = new UserInterface();
+
+/*
+ * Funciones
+ * */
+function preguntarPresupuesto() {
+  // Muestra prompt y valida el resultado
+  const presupuestoUsuario = prompt("What is your weekly budget?");
+
+  if (
+    presupuestoUsuario === "" ||
+    presupuestoUsuario === null ||
+    isNaN(presupuestoUsuario) ||
+    presupuestoUsuario <= 0
+  ) {
+    window.location.reload();
+  }
+
+  presupuesto = new Presupuesto(presupuestoUsuario); // Pasa presupuesto de prompt a la clase Presupuesto
+  console.log(presupuesto);
+}
